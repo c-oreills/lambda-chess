245ea906928a32ea9729190ab09fa27a9ad8dbee
Christy O'Reilly <christy@oreills.co.uk>
2014-05-12 22:00:21 +0100

    Random check fn

--- src/lambda_chess/core.clj
+++ src/lambda_chess/core.clj
@@ -1,16 +1,39 @@
 (ns lambda-chess.core
   (:require [clj-chess-engine.core :as chess]))
 
+(defn check? [board am-i-white?]
+  (chess/check? board (not am-i-white?) []))
+
+(def col-map
+  {\a 0 \b 1 \c 2 \d 3 \e 4 \f 5 \g 6 \h 7})
+
+(def row-map
+  {\1 0 \2 1 \3 2 \4 3 \5 4 \6 5 \7 6 \8 7})
+
+(defn convert-to-index [s]
+  (+ (* (col-map (first s)) 8) (row-map (second s))))
+
+
+(defn my-apply-move [board [from to]]
+  (let [from-i (convert-to-index from)
+        to-i (convert-to-index to)
+        piece (get board from-i)]
+    (assoc (assoc board from-i \-) to-i piece)))
+
+(defn try-check [board valid-moves am-i-white?]
+  (get valid-moves (ffirst (filter #(check? (second %) (not am-i-white?))
+                 (map-indexed (fn [idx itm] [idx (my-apply-move board itm)])
+                              valid-moves)))))
+
 ;; my-fn is the var pointing onto the function we want to test
 (def my-fn
   ;;-----------------
   ;; copy and past this anonymous function on lambda-zone.com
   (fn [{:keys [board valid-moves am-i-white? in-check? history state]}]
+    (or (let [res (try-check board valid-moves am-i-white?)] (when res (println "CHECK MATE!!! CHECK MATE!!! CHECK MATE!!! CHECK MATE!!!")) res)
                       (let [v (into [] valid-moves)]
                         (let [move (rand-int (count valid-moves))]
-                          {:move (get v move), :state nil})))
-  ;;-----------------
-)
+                          {:move (get v move), :state nil})))))
 
 
 (defn test-fn-vs-fn []
@@ -31,3 +54,4 @@
   ;;(test-human-vs-fn)
 
   )
+
